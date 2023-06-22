1bc51e8d0b1b771c380118d9b23687892d9ca007
shahab-edraak <shahab@edraaksystems.com>
2023-06-22 16:09:01 +0500

    slider

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-const primaryHeader=document.querySelector('.primary_header')
-const navToggle=document.querySelector('.mob_nav_toggle')
-const primaryNav=document.querySelector('.primary_nav')
+const primaryHeader = document.querySelector('.primary_header')
+const navToggle = document.querySelector('.mob_nav_toggle')
+const primaryNav = document.querySelector('.primary_nav')
 
-navToggle.addEventListener('click',()=>{
-    primaryNav.hasAttribute('data-visible')?navToggle.setAttribute('aria-expanded',false):navToggle.setAttribute('aria-expanded',true);
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute('data-visible') ? navToggle.setAttribute('aria-expanded', false) : navToggle.setAttribute('aria-expanded', true);
     primaryNav.toggleAttribute('data-visible');
     primaryHeader.toggleAttribute('data-overlay');
 })
+
+const slider = new A11YSlider(document.querySelector(".slider"), {
+    adaptiveHeight: false,
+    dots: true,
+    centerMode: true,
+    arrows: false,
+    responsive: {
+
+        480: {
+            dots: false, // dots enabled below 480
+        },
+    },
+});
